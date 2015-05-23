5410fbfa81f10f4a2de5660028c421b2d742d75b
art_net <art9207@gmail.com>
2015-05-23 09:34:30 +0300

    + IRleLwzCompression to RleLwzCompression form

--- RleLwzCompression/RleLwzCompression/RleLwzCompression.cs
+++ RleLwzCompression/RleLwzCompression/RleLwzCompression.cs
@@ -7,14 +7,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using RleLwzCompressionLibrary.Models;
+using RleLwzCompressionLibrary.ViewInerfaces;
 
 namespace RleLwzCompression
 {
-    public partial class RleLwzCompression : Form
+    public partial class RleLwzCompression : Form, IRleLwzCompression
     {
         public RleLwzCompression()
         {
             InitializeComponent();
+        }
+
+        public event Action ButtonLoadPicture;
+        public event Action ButtonCloseRleLwzCompressionForm;
+        public event Action ButtonEncodePicture;
+        public event Action ButtonDecodePicture;
+        public event Action ButtonClearSources;
+        public void ShowPicture(Picture picture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CloseForm()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ClearSources()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LoadPicture()
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- RleLwzCompression/RleLwzCompressionLibrary/ViewInerfaces/IRleLwzCompression.cs
+++ RleLwzCompression/RleLwzCompressionLibrary/ViewInerfaces/IRleLwzCompression.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using RleLwzCompressionLibrary.Models;
 
 namespace RleLwzCompressionLibrary.ViewInerfaces
