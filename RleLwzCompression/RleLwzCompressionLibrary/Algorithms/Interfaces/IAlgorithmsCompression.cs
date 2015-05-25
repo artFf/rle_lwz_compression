6836c3eb5497fb295a1baafb75a4d365d9a3947e
art_net <art9207@gmail.com>
2015-05-25 21:53:44 +0300

    + Rle realisation

--- RleLwzCompression/RleLwzCompressionLibrary/Algorithms/Interfaces/IAlgorithmsCompression.cs
+++ RleLwzCompression/RleLwzCompressionLibrary/Algorithms/Interfaces/IAlgorithmsCompression.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
-using RleLwzCompressionLibrary.Models;
+﻿using RleLwzCompressionLibrary.Models;
 
 namespace RleLwzCompressionLibrary.Algorithms.Interfaces
 {
     public interface IAlgorithmsCompression
     {
         Picture Encode(Picture picture);
-        Picture Decode(Picture picture);
+        Picture Decode(Picture picture, int size);
     }
 }
--- RleLwzCompression/RleLwzCompressionLibrary/Algorithms/Realisations/Rle.cs
+++ RleLwzCompression/RleLwzCompressionLibrary/Algorithms/Realisations/Rle.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 using RleLwzCompressionLibrary.Algorithms.Interfaces;
 using RleLwzCompressionLibrary.Models;
 
@@ -12,12 +8,49 @@ namespace RleLwzCompressionLibrary.Algorithms.Realisations
     {
         public Picture Encode(Picture picture)
         {
-            throw new NotImplementedException();
+            var pictureInbytes = File.ReadAllBytes(picture.Path);
+            Picture encodedPicture = new Picture
+            {
+                EncodedContents = new byte[pictureInbytes.Length*2],
+                Name = picture.Name,
+                Path = picture.Path
+            };
+
+            int ii = 0;
+            for (int i = 0; i < pictureInbytes.Length; i++)
+            {
+                byte currentBytesCount = 1;
+                while (currentBytesCount < byte.MaxValue && i + 1 < pictureInbytes.Length && pictureInbytes[i] == pictureInbytes[i + 1])
+                {
+                    currentBytesCount++;
+                    i++;
+                }
+                encodedPicture.EncodedContents[ii] = currentBytesCount;
+                ii++;
+                encodedPicture.EncodedContents[ii] = pictureInbytes[i];
+                ii++;
+            }
+            
+            encodedPicture.Size = encodedPicture.EncodedContents.Length;
+            return encodedPicture;
         }
 
-        public Picture Decode(Picture picture)
+        public Picture Decode(Picture picture, int size)
         {
-            throw new NotImplementedException();
+            var decodedPicture = picture;
+            decodedPicture.DecodedContents = new byte[size];
+            int step = 0;
+            for (int i = 1; i < picture.EncodedContents.Length; i += 2)
+            {
+                byte currentBytesCount = picture.EncodedContents[i - 1];
+                while (currentBytesCount > 0)
+                {
+                    decodedPicture.DecodedContents[step] = picture.EncodedContents[i];
+                    currentBytesCount--;
+                    step++;
+                }
+            }
+            return decodedPicture;
         }
     }
 }
