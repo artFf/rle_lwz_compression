fb7bb87fd3f6912136bf551d9587538d9e2fcb65
art_net <art9207@gmail.com>
2015-05-25 21:57:05 +0300

    + Lwz realisation has been started

--- RleLwzCompression/RleLwzCompressionLibrary/Algorithms/Realisations/Lwz.cs
+++ RleLwzCompression/RleLwzCompressionLibrary/Algorithms/Realisations/Lwz.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +13,48 @@ namespace RleLwzCompressionLibrary.Algorithms.Realisations
     {
         public Picture Encode(Picture picture)
         {
-            throw new NotImplementedException();
+            var pictureInbytes = File.ReadAllBytes(picture.Path);
+            Picture encodedPicture = new Picture
+            {
+                EncodedContents = new byte[pictureInbytes.Length],
+                Name = picture.Name,
+                Path = picture.Path
+            };
+            string byteArrayInString = pictureInbytes.ToString();
+            int dictionarySize = 256;
+            var dictionary = new Dictionary<string, int>();
+            int[] resultCode = new int[pictureInbytes.Length];
+            string codePosition = "";
+            for (int i = 0; i < dictionarySize; i++)
+            {
+                dictionary.Add(((char)i).ToString(), i);
+            }
+            int lastStep = 0;
+            for (int i = 0; i < byteArrayInString.Length; i++)
+            {
+                char simvol = (char)byteArrayInString[i];
+                string wc = codePosition + simvol;
+                if (dictionary.ContainsKey(wc))
+                {
+                    codePosition = wc;
+                }
+                else
+                {
+                    resultCode[i] = (dictionary[codePosition]);
+                    dictionary.Add(wc, dictionarySize++);
+                    codePosition = "" + simvol;
+                }
+                lastStep = i + 1;
+            }
+            if (codePosition != "")
+            {
+                resultCode[lastStep] = dictionary[codePosition];
+            }
+            //return resultCode.ToArray();
+            return encodedPicture;
         }
 
-        public Picture Decode(Picture picture)
+        public Picture Decode(Picture picture, int size)
         {
             throw new NotImplementedException();
         }
