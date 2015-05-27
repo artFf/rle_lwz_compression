9681dfa1f83546278b5f8e93ac5ee5e2bf67c177
art_net <art9207@gmail.com>
2015-05-27 09:43:25 +0300

    + LZW realisation

--- RleLwzCompression/RleLwzCompressionLibrary/Algorithms/Realisations/Lwz.cs
+++ RleLwzCompression/RleLwzCompressionLibrary/Algorithms/Realisations/Lwz.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using RleLwzCompressionLibrary.Algorithms.Interfaces;
 using RleLwzCompressionLibrary.Models;
 
@@ -11,52 +10,97 @@ namespace RleLwzCompressionLibrary.Algorithms.Realisations
 {
     public class Lwz : IAlgorithmsCompression
     {
+        //http://rosettacode.org/wiki/LZW_compression#C.23
         public Picture Encode(Picture picture)
         {
             var pictureInbytes = File.ReadAllBytes(picture.Path);
             Picture encodedPicture = new Picture
             {
                 EncodedContents = new byte[pictureInbytes.Length],
+                EncodedIntContents = new int[pictureInbytes.Length],
                 Name = picture.Name,
                 Path = picture.Path
             };
-            string byteArrayInString = pictureInbytes.ToString();
-            int dictionarySize = 256;
-            var dictionary = new Dictionary<string, int>();
-            int[] resultCode = new int[pictureInbytes.Length];
-            string codePosition = "";
-            for (int i = 0; i < dictionarySize; i++)
-            {
+
+            string uncompressed = ConvertByteArrayToString(pictureInbytes);
+            
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+            for (int i = 0; i < 256; i++)
                 dictionary.Add(((char)i).ToString(), i);
-            }
-            int lastStep = 0;
-            for (int i = 0; i < byteArrayInString.Length; i++)
+
+            string w = string.Empty;
+            List<int> compressed = new List<int>();
+
+            foreach (char c in uncompressed)
             {
-                char simvol = (char)byteArrayInString[i];
-                string wc = codePosition + simvol;
+                string wc = w + c;
                 if (dictionary.ContainsKey(wc))
                 {
-                    codePosition = wc;
+                    w = wc;
                 }
                 else
                 {
-                    resultCode[i] = (dictionary[codePosition]);
-                    dictionary.Add(wc, dictionarySize++);
-                    codePosition = "" + simvol;
+                    compressed.Add(dictionary[w]);
+                    dictionary.Add(wc, dictionary.Count);
+                    w = c.ToString();
                 }
-                lastStep = i + 1;
             }
-            if (codePosition != "")
-            {
-                resultCode[lastStep] = dictionary[codePosition];
-            }
-            //return resultCode.ToArray();
+
+            if (!string.IsNullOrEmpty(w))
+                compressed.Add(dictionary[w]);
+
+            encodedPicture.EncodedIntContents = compressed.ToArray();
+            encodedPicture.Size = encodedPicture.EncodedIntContents.Length;
             return encodedPicture;
         }
 
         public Picture Decode(Picture picture, int size)
         {
-            throw new NotImplementedException();
+            //todo
+            var decodedPicture = picture;
+            decodedPicture.DecodedContents = new byte[size];
+
+            List<int> compressed = picture.EncodedIntContents.ToList();
+            Dictionary<int, string> dictionary = new Dictionary<int, string>();
+            for (int i = 0; i < 256; i++)
+                dictionary.Add(i, ((char)i).ToString());
+
+            string w = dictionary[compressed[0]];
+            compressed.RemoveAt(0);
+            StringBuilder decompressed = new StringBuilder(w);
+            foreach (int k in compressed)
+            {
+                string entry = string.Empty;
+                if (dictionary.ContainsKey(k))
+                    entry = dictionary[k];
+                else if (k == dictionary.Count)
+                    entry = w + w[0];
+
+                decompressed.Append(entry);
+                dictionary.Add(dictionary.Count, w + entry[0]);
+                w = entry;
+            }
+            string sstr = decompressed.ToString();
+            decodedPicture.DecodedContents = ConvertStringToByteArray(sstr);
+            return decodedPicture;
+        }
+
+        private byte[] ConvertStringToByteArray(string strByteImage)
+        {
+            string[] split = strByteImage.Split(' ');
+            return (from s in split where s.Trim() != "" select Byte.Parse(s)).ToArray();
+        }
+
+        private string ConvertByteArrayToString(byte[] image)
+        {
+            return string.Join(" ", image.Select(i => i));
+            /*StringBuilder sb = new StringBuilder();
+            foreach (byte t in image)
+            {
+                sb.Append(t.ToString() + " ");
+                //sb.Append(" ");
+            }
+            return sb.ToString();*/
         }
     }
 }
