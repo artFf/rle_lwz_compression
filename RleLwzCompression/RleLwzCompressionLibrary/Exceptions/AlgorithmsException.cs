19c54bb65aa48b7c0b65eb37b4b44c6fb281c58b
art_net <art9207@gmail.com>
2015-07-25 10:43:10 +0300

    + AlgorithmsException

---
+++ RleLwzCompression/RleLwzCompressionLibrary/Exceptions/AlgorithmsException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RleLwzCompressionLibrary.Exceptions
+{
+    public class AlgorithmsException : Exception
+    {
+        public AlgorithmsException() { }
+
+        public AlgorithmsException(string message) : base(message) { }
+
+        public AlgorithmsException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
