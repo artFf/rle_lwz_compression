f83f5d1c6dcb29c98dc9d0678754aa380e6a9e10
art_net <art9207@gmail.com>
2015-07-28 08:44:22 +0300

    + add tests

---
+++ RleLwzCompression/RleLwzCompressionTestProject/AlgorithmsTest.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RleLwzCompression;
+using RleLwzCompressionLibrary.Algorithms.Contexts;
+using RleLwzCompressionLibrary.Algorithms.Realisations;
+using RleLwzCompressionLibrary.Models;
+using RleLwzCompressionLibrary.SimpleFactory;
+
+namespace RleLwzCompressionTestProject
+{
+    [TestClass]
+    public class AlgorithmsTest
+    {
+        [TestMethod]
+        public void TestRleGetEncodedPicture()
+        {
+            //act
+            Context context = new Context(AlgorithmsFactory.CreateInstance<Rle>());
+            string pathToPicture = Environment.CurrentDirectory + @"\black-and-white-wallpaper-3.jpg";            
+            Picture loadPicture = new Picture();
+            Picture encodedPicture = new Picture();
+            RleLwzCompressionForm rleLwzCompressionForm = new RleLwzCompressionForm();
+
+            //arrange
+            loadPicture = rleLwzCompressionForm.GetPictureInfo(pathToPicture);
+            encodedPicture = context.ExuceteEncode(loadPicture);
+            //assert
+            Assert.IsNotNull(loadPicture);
+            Assert.IsNotNull(encodedPicture);
+            Assert.IsNull(loadPicture.EncodedContents);
+            Assert.IsNull(loadPicture.DecodedContents);
+            Assert.IsNotNull(encodedPicture.EncodedContents);
+            Assert.IsNull(encodedPicture.DecodedContents);
+            Assert.AreEqual(loadPicture.Path,encodedPicture.Path);
+            Assert.AreEqual(loadPicture.Name,encodedPicture.Name);
+            Assert.AreNotEqual(loadPicture.Size,encodedPicture.Size);
+        }
+
+        [TestMethod]
+        public void TestLwzGetEncodedPicture()
+        {
+            //act
+            Context context = new Context(AlgorithmsFactory.CreateInstance<Lwz>());
+            string pathToPicture = Environment.CurrentDirectory + @"\black-and-white-wallpaper-3.jpg";
+            Picture loadPicture = new Picture();
+            Picture encodedPicture = new Picture();
+            RleLwzCompressionForm rleLwzCompressionForm = new RleLwzCompressionForm();
+
+            //arrange
+            loadPicture = rleLwzCompressionForm.GetPictureInfo(pathToPicture);
+            encodedPicture = context.ExuceteEncode(loadPicture);
+            //assert
+            Assert.IsNotNull(loadPicture);
+            Assert.IsNotNull(encodedPicture);
+            Assert.IsNull(loadPicture.EncodedContents);
+            Assert.IsNull(loadPicture.DecodedContents);
+            Assert.IsNotNull(encodedPicture.EncodedContents);
+            Assert.IsNull(encodedPicture.DecodedContents);
+            Assert.AreEqual(loadPicture.Path, encodedPicture.Path);
+            Assert.AreEqual(loadPicture.Name, encodedPicture.Name);
+            Assert.AreNotEqual(loadPicture.Size, encodedPicture.Size);
+        }
+
+        [TestMethod]
+        public void TestRleGetDecodedPicture()
+        {
+            //act
+            Context context = new Context(AlgorithmsFactory.CreateInstance<Rle>());
+            string pathToPicture = Environment.CurrentDirectory + @"\black-and-white-wallpaper-3.jpg";
+            Picture loadPicture = new Picture();
+            Picture decodedPicture = new Picture();
+            Picture encodedPicture = new Picture();
+            RleLwzCompressionForm rleLwzCompressionForm = new RleLwzCompressionForm();
+
+            //arrange
+            loadPicture = rleLwzCompressionForm.GetPictureInfo(pathToPicture);
+            encodedPicture = context.ExuceteEncode(loadPicture);
+            decodedPicture = context.ExuceteDecode(encodedPicture);
+            //assert
+            Assert.IsNotNull(loadPicture);
+            Assert.IsNotNull(decodedPicture);
+            Assert.IsNull(loadPicture.EncodedContents);
+            Assert.IsNull(loadPicture.DecodedContents);
+            Assert.IsNotNull(decodedPicture.EncodedContents);
+            Assert.IsNotNull(decodedPicture.DecodedContents);
+            Assert.AreEqual(loadPicture.Path, decodedPicture.Path);
+            Assert.AreEqual(loadPicture.Name, decodedPicture.Name);
+            Assert.AreEqual(loadPicture.Size, decodedPicture.Size);
+        }
+
+        [TestMethod]
+        public void TestLwzGetDecodedPicture()
+        {
+            //act
+            Context context = new Context(AlgorithmsFactory.CreateInstance<Lwz>());
+            string pathToPicture = Environment.CurrentDirectory + @"\black-and-white-wallpaper-3.jpg";
+            Picture loadPicture = new Picture();
+            Picture decodedPicture = new Picture();
+            Picture encodedPicture = new Picture();
+            RleLwzCompressionForm rleLwzCompressionForm = new RleLwzCompressionForm();
+
+            //arrange
+            loadPicture = rleLwzCompressionForm.GetPictureInfo(pathToPicture);
+            encodedPicture = context.ExuceteEncode(loadPicture);
+            decodedPicture = context.ExuceteDecode(encodedPicture);
+            //assert
+            Assert.IsNotNull(loadPicture);
+            Assert.IsNotNull(decodedPicture);
+            Assert.IsNull(loadPicture.EncodedContents);
+            Assert.IsNull(loadPicture.DecodedContents);
+            Assert.IsNotNull(decodedPicture.EncodedContents);
+            Assert.IsNotNull(decodedPicture.DecodedContents);
+            Assert.AreEqual(loadPicture.Path, decodedPicture.Path);
+            Assert.AreEqual(loadPicture.Name, decodedPicture.Name);
+            Assert.AreEqual(loadPicture.Size, decodedPicture.Size);
+        }
+    }
+}
---
+++ RleLwzCompression/RleLwzCompressionTestProject/PictureTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RleLwzCompression;
+using RleLwzCompressionLibrary.Enums;
+using RleLwzCompressionLibrary.Models;
+
+namespace RleLwzCompressionTestProject
+{
+    [TestClass]
+    public class PictureTest
+    {
+        [TestMethod]
+        public void TestGetLoadedPictureInfo()
+        {
+            //act
+            string pathToPicture = Environment.CurrentDirectory + @"\black-and-white-wallpaper-3.jpg";
+            const int size = 253663;
+            Picture testPicture = new Picture();
+            RleLwzCompressionForm rleLwzCompressionForm = new RleLwzCompressionForm();
+
+            //arrange
+            testPicture = rleLwzCompressionForm.GetPictureInfo(pathToPicture);
+
+            //assert
+            Assert.IsNotNull(testPicture);
+            Assert.AreEqual(pathToPicture, testPicture.Path);
+            Assert.AreEqual("black-and-white-wallpaper-3.jpg", testPicture.Name);
+            Assert.AreEqual(size, testPicture.Size);
+            Assert.IsNull(testPicture.EncodedContents);
+            Assert.IsNull(testPicture.DecodedContents);
+        }
+
+        [TestMethod]
+        public void TestLoadPicture()
+        {
+            //act
+            string pathToPicture = Environment.CurrentDirectory + @"\black-and-white-wallpaper-3.jpg";
+            string pathToLoadedPicture = string.Empty;
+            RleLwzCompressionForm rleLwzCompressionForm = new RleLwzCompressionForm();
+
+            //arrange
+            pathToLoadedPicture = rleLwzCompressionForm.LoadPicture();
+
+            //assert
+            Assert.IsNotNull(pathToLoadedPicture);
+            Assert.AreEqual(pathToPicture, pathToLoadedPicture);
+        }
+    }
+}
